47d42c822b6e35875bc8f7a43f124f1f7e25c72e
Felo Vilches <felovilches@gmail.com>
2017-10-01 20:46:13 -0300

    refactorizacion

--- spec/models/branch_office_spec.rb
+++ spec/models/branch_office_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe BranchOffice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "valida que este asociado a una comuna" do
+    expect(FactoryGirl.build(:branch_office)).to be_valid
+    expect(FactoryGirl.build(:branch_office, :comuna => nil)).to_not be_valid
+  end
+
+  it "valida que tenga direccion" do
+    expect(FactoryGirl.build(:branch_office, :address => "")).to_not be_valid
+    expect(FactoryGirl.build(:branch_office, :address => "   ")).to_not be_valid
+    expect(FactoryGirl.build(:branch_office, :address => "\t\n\t")).to_not be_valid
+    expect(FactoryGirl.build(:branch_office, :address => "\ta\n\t")).to be_valid
+  end
+
+  it "elimina espacios redundantes en la direccion" do
+    office = FactoryGirl.create(:branch_office, :address => "\n\na \t  b c\n    \td  ")
+    expect(office.address).to eq 'a b c d'
+  end
+
 end
--- spec/models/comuna_spec.rb
+++ spec/models/comuna_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comuna, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "valida que este asociado a una region" do
+    expect(FactoryGirl.build(:comuna)).to be_valid
+    expect(FactoryGirl.build(:comuna, :region => nil)).to_not be_valid
+  end
 end
