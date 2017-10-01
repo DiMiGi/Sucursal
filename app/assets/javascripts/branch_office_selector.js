777805dac5fb376fbc941903e7fa0f2e239d6c28
Felo Vilches <felovilches@gmail.com>
2017-09-30 22:58:47 -0300

    hice que el selector de sucursal fuera reutilizable

---
+++ app/assets/javascripts/branch_office_selector.js
@@ -0,0 +1,86 @@
+var BranchOfficeSelector = function(jQueryObj, loadingMessage = "Obteniendo sucursales..."){
+
+
+  jQueryObj.html('<span class="glyphicon glyphicon-refresh spinning"></span> ' + loadingMessage);
+
+
+  var offices = {};
+  var spinner = jQueryObj.find("#obteniendo-sucursales");
+
+
+  function showAddresses(region, comuna){
+    var select = jQueryObj.find("#address-select");
+    select.empty();
+    var addr = offices[region][comuna];
+    for(var i in addr){
+      select.append('<option value="' + addr[i].address + '">' + addr[i].address + '</option>');
+    }
+  }
+
+  function showComunas(region){
+    var select = jQueryObj.find("#comuna-select");
+    select.empty();
+    for(var key in offices[region]){
+      select.append('<option value="' + key + '">' + key + '</option>');
+    }
+    showAddresses(region, select.val());
+  }
+
+  function showAllRegions(){
+    var select = jQueryObj.find("#region-select");
+    select.empty();
+    for(var key in offices){
+      select.append('<option value="' + key + '">' + key + '</option>');
+    }
+    showComunas(select.val());
+  }
+
+  this.obtenerSucursal = function(){
+    var region = jQueryObj.find("#region-select").val();
+    var comuna = jQueryObj.find("#comuna-select").val();
+    var address = jQueryObj.find("#address-select").val();
+    var result;
+
+    for(i in offices[region][comuna]){
+      if(offices[region][comuna][i].address == address){
+        result = offices[region][comuna][i];
+      }
+    }
+
+    return { id: result.id, address: result.address, region, comuna };
+  }
+
+  function showSelects(){
+    jQueryObj.empty();
+    jQueryObj.append('<select class="form-control" id="region-select"></select>');
+    jQueryObj.append('<select class="form-control" id="comuna-select"></select>');
+    jQueryObj.append('<select class="form-control" id="address-select"></select>');
+
+    jQueryObj.find("#comuna-select").change(function(){
+      showAddresses(jQueryObj.find("#region-select").val(), $(this).val());
+    });
+
+    jQueryObj.find("#region-select").change(function(){
+      showComunas($(this).val());
+    });
+
+    showAllRegions();
+  }
+
+
+  $.ajax({
+    url: "/regions/comunas/branch_offices",
+    method: 'GET',
+    success: function(res){
+      offices = res;
+      spinner.hide();
+      showSelects();
+    },
+    error: function(err){
+      $.notify("Error obteniendo las sucursales", "error");
+      //spinner.hide();
+    }
+  });
+
+
+}
--- app/assets/javascripts/staff.js
+++ app/assets/javascripts/staff.js
@@ -2,75 +2,15 @@ $(document).ready(function(){
 
   $("#branch-office-list-by-location").each(function(){
 
-    var offices = {};
-    var spinner = $("#obteniendo-sucursales");
-
-    $("#comuna-select").change(function(){
-      showAddresses($("#region-select").val(), $("#comuna-select").val());
-    });
-
-    $("#region-select").change(function(){
-      showComunas($("#region-select").val());
-    });
-
-    function showAddresses(region, comuna){
-      var select = $("#address-select");
-      select.empty();
-      var addr = offices[region][comuna];
-      for(var i in addr){
-        select.append('<option value="' + addr[i] + '">' + addr[i].address + '</option>');
-      }
-    }
-
-    function showComunas(region){
-      var select = $("#comuna-select");
-      select.empty();
-      for(var key in offices[region]){
-        select.append('<option value="' + key + '">' + key + '</option>');
-      }
-      showAddresses(region, select.val());
-    }
-
-    function showAllRegions(){
-      var select = $("#region-select");
-      select.empty();
-      for(var key in offices){
-        select.append('<option value="' + key + '">' + key + '</option>');
-      }
-      showComunas(select.val());
-    }
-
-    function showSelects(){
-      $("#region-select").show();
-      $("#comuna-select").show();
-      $("#address-select").show();
-    }
+    var selectorSucursal = new BranchOfficeSelector($("#selector-sucursales"));
 
     $("#save-user").click(function(){
-      console.log("Guardar el archivo")
+
+      console.log(selectorSucursal.obtenerSucursal());
 
     });
 
-
-    $.ajax({
-      url: "/regions/comunas/branch_offices",
-      method: 'GET',
-      success: function(res){
-        offices = res;
-        spinner.hide();
-        showSelects();
-        showAllRegions();
-
-      },
-      error: function(err){
-        $.notify("Error obteniendo las sucursales", "error");
-        //spinner.hide();
-      }
-    });
 
 
   });
-
-
-
 });
