6c98551b5be690c023c71d39229836d0ce16f8bd
Felo Vilches <felovilches@gmail.com>
2017-10-03 18:09:09 -0300

    appointments permite buscar por dia y otros atributos (ejecutivo, etc)

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -31,6 +31,42 @@ RSpec.describe Appointment, type: :model do
 
   end
 
+  it "permite buscar por dia correctamente, y ademas con otros atributos" do
+    ex1 = FactoryGirl.create(:executive)
+    ex2 = FactoryGirl.create(:executive)
+    ex3 = FactoryGirl.create(:executive)
+    ex4 = FactoryGirl.create(:executive)
+    Appointment.destroy_all
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 1, 1, 13, 41, 05), :executive => ex1)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 1, 1, 13, 41, 05), :executive => ex2)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 1, 2, 13, 41, 05), :executive => ex2)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 2, 3, 0, 0, 0), :executive => ex1)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 2, 3, 13, 1, 13), :executive => ex1)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 2, 3, 23, 2, 47), :executive => ex1)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 2, 3, 13, 41, 05), :executive => ex2)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 2, 3, 23, 59, 59), :executive => ex3)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 2, 4, 0, 0, 0), :executive => ex4)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 2, 5, 0, 0, 0), :executive => ex4)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 5, 5, 0, 0, 0), :executive => ex3)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 5, 5, 23, 59, 59), :executive => ex4)
+    FactoryGirl.create(:appointment, :time => DateTime.new(2017, 5, 6, 0, 0, 0), :executive => ex3)
+
+    expect(Appointment.find_by_day(Date.new(2017, 1, 1)).where(:executive => ex1).count).to eq 1
+    expect(Appointment.find_by_day(Date.new(2017, 1, 2)).where(:executive => ex1).count).to eq 0
+    expect(Appointment.find_by_day(Date.new(2017, 1, 2)).where(:executive => ex2).count).to eq 1
+    expect(Appointment.find_by_day(Date.new(2017, 2, 3)).where(:executive => ex2).count).to eq 1
+    expect(Appointment.find_by_day(Date.new(2017, 2, 3)).where(:executive => ex1).count).to eq 3
+    expect(Appointment.find_by_day(Date.new(2017, 2, 1)).where(:executive => ex1).count).to eq 0
+    expect(Appointment.find_by_day(Date.new(2017, 5, 5)).where(:executive => ex1).count).to eq 0
+    expect(Appointment.find_by_day(Date.new(2017, 5, 5)).where(:executive => ex2).count).to eq 0
+    expect(Appointment.find_by_day(Date.new(2017, 5, 5)).where(:executive => ex3).count).to eq 1
+    expect(Appointment.find_by_day(Date.new(2017, 5, 5)).where(:executive => ex4).count).to eq 1
+    expect(Appointment.find_by_day(Date.new(2017, 5, 6)).where(:executive => ex3).count).to eq 1
+
+  end
+
+
+
 
   it "los tiempos se discretizan correctamente" do
 
