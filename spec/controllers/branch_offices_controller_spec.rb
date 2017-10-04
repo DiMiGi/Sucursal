489c7e6d31e79e385994e20b6a9201c6e03d4ddb
Felo Vilches <felovilches@gmail.com>
2017-10-03 22:53:55 -0300

    arregle unos tests

--- spec/controllers/branch_offices_controller_spec.rb
+++ spec/controllers/branch_offices_controller_spec.rb
@@ -2,67 +2,66 @@ require 'rails_helper'
 
 RSpec.describe BranchOfficesController, type: :controller do
 
-  before(:all) do
-    FactoryGirl.create(:attention_type)
-    FactoryGirl.create(:attention_type)
-    FactoryGirl.create(:attention_type)
-    FactoryGirl.create(:attention_type)
-    FactoryGirl.create(:attention_type)
-    FactoryGirl.create(:attention_type)
-  end
-
-
   describe "modificacion de las estimaciones que le da una sucursal a un tipo de atencion" do
 
-      it "reemplaza la lista de estimaciones correctamente" do
+    before(:all) do
+      @a1 = FactoryGirl.create(:attention_type)
+      @a2 = FactoryGirl.create(:attention_type)
+      @a3 = FactoryGirl.create(:attention_type)
+      @a4 = FactoryGirl.create(:attention_type)
+      @a5 = FactoryGirl.create(:attention_type)
+      @a6 = FactoryGirl.create(:attention_type)
+    end
 
-        staff = FactoryGirl.create(:supervisor)
-        sign_in staff
 
-        # Creo dos listas de estimaciones, con distinto largo, y una ID de atencion igual en ambas listas
-        est1 = [{ "attention_type_id": 1 , "duration": 30}, { "attention_type_id": 2 , "duration": 15},
-    		{ "attention_type_id": 3 , "duration": 15}]
+    it "reemplaza la lista de estimaciones correctamente" do
 
-        est2 = [{ "attention_type_id": 3 , "duration": 30}, { "attention_type_id": 5 , "duration": 45}]
+      staff = FactoryGirl.create(:supervisor)
+      sign_in staff
 
-        branch_office = BranchOffice.find(staff.branch_office_id)
-        expect(branch_office.duration_estimations.length).to eq 0
+      # Creo dos listas de estimaciones, con distinto largo, y una ID de atencion igual en ambas listas
+      est1 = [{ "attention_type_id": @a1.id , "duration": 30}, { "attention_type_id": @a2.id , "duration": 15},
+  		{ "attention_type_id": @a3.id , "duration": 15}]
 
-        duration_estimation_count = DurationEstimation.count
+      est2 = [{ "attention_type_id": @a3.id , "duration": 30}, { "attention_type_id": @a5.id , "duration": 45}]
 
+      branch_office = BranchOffice.find(staff.branch_office_id)
+      expect(branch_office.duration_estimations.length).to eq 0
+
+      expect{
         # Hacer la primera actualizacion
         put :update_attention_types_estimations, params: { :id => branch_office.id, :duration_estimations => est1 }
         expect(response).to have_http_status(:ok)
         branch_office.reload
         expect(branch_office.duration_estimations.length).to eq 3
-        expect(branch_office.duration_estimations[0].attention_type_id).to eq 1
-        expect(branch_office.duration_estimations[1].attention_type_id).to eq 2
-        expect(branch_office.duration_estimations[2].attention_type_id).to eq 3
+        expect(branch_office.duration_estimations[0].attention_type_id).to eq @a1.id
+        expect(branch_office.duration_estimations[1].attention_type_id).to eq @a2.id
+        expect(branch_office.duration_estimations[2].attention_type_id).to eq @a3.id
         expect(branch_office.duration_estimations[0].branch_office_id).to eq branch_office.id
         expect(branch_office.duration_estimations[1].branch_office_id).to eq branch_office.id
         expect(branch_office.duration_estimations[2].branch_office_id).to eq branch_office.id
-        expect(DurationEstimation.count).to eq(duration_estimation_count+3)
+      }.to change(DurationEstimation, :count).by(3)
 
+      expect{
         # Hacer la segunda actualizacion
         put :update_attention_types_estimations, params: { :id => branch_office.id, :duration_estimations => est2 }
         expect(response).to have_http_status(:ok)
         branch_office.reload
         expect(branch_office.duration_estimations.length).to eq 2
-        expect(branch_office.duration_estimations[0].attention_type_id).to eq 3
+        expect(branch_office.duration_estimations[0].attention_type_id).to eq @a3.id
         expect(branch_office.duration_estimations[0].duration).to eq 30
-        expect(branch_office.duration_estimations[1].attention_type_id).to eq 5
+        expect(branch_office.duration_estimations[1].attention_type_id).to eq @a5.id
         expect(branch_office.duration_estimations[1].duration).to eq 45
         expect(branch_office.duration_estimations[0].branch_office_id).to eq branch_office.id
         expect(branch_office.duration_estimations[1].branch_office_id).to eq branch_office.id
+      }.to change(DurationEstimation, :count).by(-1) # Se eliminaron las anteriores y ahora hay solo 2
 
-        # Se borran las 3 anteriores y solo quedan 2
-        expect(DurationEstimation.count).to eq(duration_estimation_count + 2)
 
-        # No queda ninguna con ID null (prevenir fallas de integridad)
-        expect(DurationEstimation.where(:branch_office_id => nil).count).to eq 0
-
-      end
+      # No queda ninguna con ID null (prevenir fallas de integridad)
+      expect(DurationEstimation.where(:branch_office_id => nil).count).to eq 0
 
     end
+
+  end
 
 end
