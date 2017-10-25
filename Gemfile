aa4b4e3246450cd1cbd8d28c91385e8889fcad9f
Diego Miranda Gipoulou <diego.miranda.g@usach.cl>
2017-10-25 20:15:02 -0300

    Se agrega funcionalidad de traspaso de horas de ejecutivos
    Se cambio el gemfile se agrego platform: :ruby a la gema 'bcrypt' y un tip en caso de que se obtenga un error al usar 'rails s' en consola. Se agrega al modelo Executive funciones de administración de traspaso de horas (appointments). Ademas se incluyen una prueba de las funciones para validar que funciona en el spec

--- Gemfile
+++ Gemfile
@@ -30,7 +30,10 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-gem 'bcrypt', '~> 3.1.7'
+# CORRER EN CONSOLA => gem uninstall 'bcrypt'
+# =>                   gem install 'bcrypt' --platform=ruby
+#gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7', platforms: :ruby
 gem 'devise'
 gem "auto_strip_attributes", "~> 2.1"
 gem 'pundit'
--- app/models/executive.rb
+++ app/models/executive.rb
@@ -9,4 +9,26 @@ class Executive < Staff
 
   validates_presence_of :branch_office
 
+
+  # Método para la logica de reasignacion de horas (No implemetado pero se considera la lógica)
+  def reassign_appointments_to(replacement_executive)
+    return false if !replacement_executive.is_a?(Executive)
+    return false if !can_assign_appointmnets_to?(replacement_executive)
+    add_appointments_to(replacement_executive, appointments)
+    return replacement_executive.save
+  end
+
+  # Método encargado de manejar la logica de implementacion de insercion de las horas agendadas (no implementado con ejecutivos que contengan horarios)
+  def add_appointments_to(excecutive,appointments)
+    return false if !excecutive.is_a?(Executive)
+    #return false if !appointments.is_a?(Appointment)
+    excecutive.appointments << appointments
+    return true
+  end
+
+  # Método que debe verificar que es valido asignar horas al ejecutivo desde el objeto (self)
+  def can_assign_appointmnets_to?(executive)
+    return true
+  end
+  
 end
