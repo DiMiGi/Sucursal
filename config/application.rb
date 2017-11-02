7aa80b7926942ded8445b5ee3d06c2a65318ba14
Felo Vilches <felovilches@gmail.com>
2017-11-02 08:52:32 -0300

    cors

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,13 @@ module Sucursal
     #config.active_record.default_timezone = :local
     config.time_zone = 'Santiago'
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
